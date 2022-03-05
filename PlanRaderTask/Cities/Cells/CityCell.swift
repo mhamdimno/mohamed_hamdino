9ddef3e0ef9050adad8e50584a8ed8f64b69599e
Mhamdino95 <mhamdino95@gmail.com>
2022-03-05 08:12:33 +0200

    working with cities design

---
+++ PlanRaderTask/Cities/Cells/CityCell.swift
@@ -0,0 +1,22 @@
+//
+//  CityCell.swift
+//  PlanRaderTask
+//
+//  Created by A One Way To Allah on 05/03/2022.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+
+    @IBOutlet private weak var ciryNameLbl: UILabel!
+    @IBOutlet private weak var showInfoImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+}
