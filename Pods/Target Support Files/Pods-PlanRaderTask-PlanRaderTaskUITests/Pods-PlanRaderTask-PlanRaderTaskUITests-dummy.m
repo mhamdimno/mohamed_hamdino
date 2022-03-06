80750ffbbd5d1fc4ccea98569b72648600686d14
Mhamdino95 <mhamdino95@gmail.com>
2022-03-06 11:20:32 +0200

    fire project 🔥

---
+++ Pods/Target Support Files/Pods-PlanRaderTask-PlanRaderTaskUITests/Pods-PlanRaderTask-PlanRaderTaskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlanRaderTask_PlanRaderTaskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PlanRaderTask_PlanRaderTaskUITests
+@end
---
+++ Pods/Target Support Files/Pods-PlanRaderTaskTests/Pods-PlanRaderTaskTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlanRaderTaskTests : NSObject
+@end
+@implementation PodsDummy_Pods_PlanRaderTaskTests
+@end
