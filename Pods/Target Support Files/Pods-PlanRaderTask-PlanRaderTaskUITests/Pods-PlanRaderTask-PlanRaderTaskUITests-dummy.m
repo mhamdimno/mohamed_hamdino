3b4de2b3ab3b2a173710c3b26a37fa56479a9b31
Mhamdino95 <mhamdino95@gmail.com>
2022-03-06 11:31:05 +0200

    fire project 🔥

--- Pods/Target Support Files/Pods-PlanRaderTask-PlanRaderTaskUITests/Pods-PlanRaderTask-PlanRaderTaskUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlanRaderTask_PlanRaderTaskUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PlanRaderTask_PlanRaderTaskUITests
-@end
